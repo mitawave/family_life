8c5f3d2fec9c0f5ca59a90bc9784ca1297913407
mitawave <mwave@ax.em-net.ne.jp>
2012-03-11 23:13:38 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyLife::Application.config.secret_token = 'f4bc01191d98dd956b67c27e534c4101b571ff62df4ffa99b2b010115cabdf3daf1be428e917e3fb780291ff9357b06c7810dc7b5f09e111d0833088511b881a'
